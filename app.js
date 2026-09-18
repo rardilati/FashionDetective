628fec0203ce1dfe210704c6dce88eed6b8098f0
rardilati <rardilati92@gmail.com>
2026-09-18 08:29:17 +0200

    Prueba1 Buscador

--- app.js
+++ app.js
@@ -1,9 +1,62 @@
 // console.log("Fashion detective está funcionando");
 
+/*Prueba de que el botón de búsqueda funciona
+
 const searchButton = document.getElementById("searchButton");
 
 searchButton.addEventListener("click", function () {
 
     console.log("🔎 El detective quiere buscar");
+
+}); */
+
+
+// ELEMENTOS DEL BUSCADOR
+
+
+const searchButton = document.getElementById("searchButton");
+
+const searchPanel = document.getElementById("searchPanel");
+
+const closeSearch = document.getElementById("closeSearch");
+
+const searchInput = document.getElementById("searchInput");
+
+const performSearch = document.getElementById("performSearch");
+
+
+
+// ABRIR BUSCADOR
+
+
+searchButton.addEventListener("click", function () {
+
+    searchPanel.classList.add("active");
+
+    searchInput.focus();
+
+});
+
+
+
+// CERRAR BUSCADOR
+
+
+closeSearch.addEventListener("click", function () {
+
+    searchPanel.classList.remove("active");
+
+});
+
+
+
+// REALIZAR BÚSQUEDA
+
+
+performSearch.addEventListener("click", function () {
+
+    const searchText = searchInput.value;
+
+    console.log("🔎 Búsqueda:", searchText);
 
 });
